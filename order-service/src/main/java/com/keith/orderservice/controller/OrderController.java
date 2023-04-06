aeb1535f6051224e7b3a6a6bf89037aae8031a64
mzazakeith <mzazakeith@gmail.com>
2023-04-06 10:01:58 +0300

    reselience as a circuit breaker

--- order-service/src/main/java/com/keith/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/keith/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.keith.orderservice.controller;
 
 import com.keith.orderservice.dto.OrderRequest;
 import com.keith.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,13 @@ public class OrderController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Oops! Something went wrong please try again later";
     }
 }
